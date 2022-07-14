dc01e14aead7564f899c311312ae3e109dfc394d
samivaida <109146530+samivaida@users.noreply.github.com>
2022-07-14 08:36:06 +0000

    Add files via upload

---
+++ esercizio1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+
+int numero intero;
+int incremento;
+int decremento;
+
+{
+printf ("\nInserire un nummero intero\n");
+scanf ("%d", &numero_intero);
+
+incremento = numero_intero++;
+Decremento = numero_intero--;
+
+printf ("\nIncremento e di: %d\n", Incremento); 
+printf ("\nDecremento e di: %d\n", Decremento);
+
+}
+
+
